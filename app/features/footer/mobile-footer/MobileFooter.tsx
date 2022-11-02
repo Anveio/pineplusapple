d5e35a72e1311f920d88e4c3b6360c387f743576
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-11-01 23:58:55 -0700

    Add stylings to mobile footer

--- app/features/footer/mobile-footer/MobileFooter.tsx
+++ app/features/footer/mobile-footer/MobileFooter.tsx
@@ -9,31 +9,35 @@ const MobileFooter: React.FC = () => {
             <div className="grid grid-cols-2 gap-5">
                 <div className="flex flex-col">
                     <span className="font-bold mb-1">Helpful Links</span>
-                    <span>FAQ</span>
-                    <span>Common Issues</span>
-                    <span>Shipping & Returns</span>
-                    <span>Refer a Friend</span>
+                    <span className="hover:text-emerald-400">FAQ</span>
+                    <span className="hover:text-emerald-400">Common Issues</span>
+                    <span className="hover:text-emerald-400">Shipping & Returns</span>
+                    <span className="hover:text-emerald-400">Refer a Friend</span>
                 </div>
                 <div className="flex flex-col">
                     <span className="font-bold mb-1">Company</span>
-                    <span>Our Story</span>
-                    <span>Contact Us</span>
-                    <span>Plant Care Blog</span>
+                    <span className="hover:text-emerald-400">Our Story</span>
+                    <span className="hover:text-emerald-400">Contact Us</span>
+                    <span className="hover:text-emerald-400">Plant Care Blog</span>
                 </div>
             </div>
-            <div className="flex flex-col">
+            <div className="flex flex-col gap-1">
                 <span className="font-bold mb-1">Compliance</span>
-                <span>Accessibility Statement</span>
-                <span>Privacy Policy</span>
-                <span>Terms of Use</span>
+                <span className="hover:text-emerald-400">Accessibility Statement</span>
+                <span className="hover:text-emerald-400">Privacy Policy</span>
+                <span className="hover:text-emerald-400">Terms of Use</span>
             </div>
-            <hr></hr>
             <div className="flex flex-row gap-6">
-                <span>Connect with us</span>
+                <span className="font-bold mb-1">Connect with us</span>
                 <div className="flex flex-row items-center gap-2"><a href="https://www.instagram.com" target={"_blank"} rel="noreferrer"><InstagramLogo size={24} className="hover:text-emerald-400" /></a>
                     <a href="blossm"><Storefront size={24} className="hover:text-emerald-400" /></a>
                 </div>
             </div>
+            <hr></hr>
+            <span className="flex flex-col gap-1 pt-2 pb-5 text-xs justify-between">
+                <span>Pine + Apple © 2022 All Rights Reserved</span>
+                <span>Seattle, WA 98101, USA</span>
+            </span>
         </div>
     );
 };
