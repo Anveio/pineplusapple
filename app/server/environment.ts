a1985b70061797ddca2e84370fa5c4f38bfb8d3c
Shovon Hasan <anveio@users.noreply.github.com>
2022-11-02 01:05:17 -0700

    Improve error message when missing SESSION_SECRET.

--- app/server/environment.ts
+++ app/server/environment.ts
@@ -1,6 +1,11 @@
 import { z } from "zod";
 
-const EnvironmentString = z.string();
+const EnvironmentString = z.string({
+  required_error:
+    "You forgot to set the SESSION_SECRET environment variable. Run `fly secrets set SESSION_SECRET=$(openssl rand -hex 32) --app pineplusapple` to set it",
+  invalid_type_error:
+    "You forgot to set the SESSION_SECRET environment variable. Run `fly secrets set SESSION_SECRET=$(openssl rand -hex 32) --app pineplusapple` to set it",
+});
 
 export const SESSION_SECRET = EnvironmentString.parse(
   process.env.SESSION_SECRET
